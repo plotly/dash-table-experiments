580af82bd72fbce1970f4cca05c64e31535d7780
Chris Parmer <chris@plot.ly>
2017-11-27 13:19:52 -0500

    Revert "comparer didn't return 0 for equal elements"
    This reverts commit 824caef1a1015238bceecb237c648bf568450e8d.

--- src/data/sort.js
+++ src/data/sort.js
@@ -1,9 +1,15 @@
 import R from 'ramda';
 
 export default function sortRows(rows, sortColumn, sortDirection) {
-    switch (sortDirection) {
-        case 'ASC': return R.sort(R.ascend(R.nth(sortColumn)), rows.slice(0));
-        case 'DESC': return R.sort(R.descend(R.nth(sortColumn)), rows.slice(0));
-        default: return rows
+    function comparer(a, b) {
+        if (sortDirection === 'ASC') {
+            return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+        } else if (sortDirection === 'DESC') {
+            return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+        }
     }
+
+    return (sortDirection === 'NONE' ?
+        rows : R.sort(comparer, rows.slice(0))
+    );
 }
